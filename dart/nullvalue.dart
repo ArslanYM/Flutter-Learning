0f894933e8ef13078f447c8fc93f6b20edd7ecf4
ArslanYM <arsalanyaqmalik@gmail.com>
2023-03-29 21:57:21 +0530

    text

--- dart/nullvalue.dart
+++ dart/nullvalue.dart
@@ -6,6 +6,6 @@ main() {
   print(a);//null
 
   int b;
-  print(b); // Error: Non-nullable variable 'b' must be assigned before it can be used.
+  //print(b); Error: Non-nullable variable 'b' must be assigned before it can be used.
 
 }
