3c60d9929a98376c39f51316766a6fb1c8e81260
ArslanYM <arsalanyaqmalik@gmail.com>
2023-03-31 20:48:52 +0530

    i am rich app

---
+++ Flutter-apps/flutter_application_1/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text("I am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage()
+          ),
+        ),
+      ),
+    ),
+  );
+}
