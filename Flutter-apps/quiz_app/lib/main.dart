2aa827fd0d69582e0903d681bfe51f9b2664bafe
ArslanYM <arsalanyaqmalik@gmail.com>
2023-04-06 22:12:48 +0530

    started quiz app

---
+++ Flutter-apps/quiz_app/lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'quizbrain.dart';
+
+Quizbrain quizBrain = Quizbrain();
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  int qNumber = 0;
+  List<Icon> scoreKeeper = [];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 6,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.questionBank[qNumber].questionText,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              child: Container(
+                width: 200,
+                height: 200,
+                alignment: Alignment.center,
+                color: Colors.green,
+                child: Text(
+                  'True',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+              onPressed: () {
+                bool correctAnswer =
+                    quizBrain.questionBank[qNumber].questionAnswer;
+                if (correctAnswer == true) {
+                  print("correct ans");
+                } else {
+                  print("wrong ans");
+                }
+                setState(() {
+                  qNumber++;
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              child: Container(
+                width: 200,
+                height: 200,
+                alignment: Alignment.center,
+                color: Colors.red,
+                child: Text(
+                  'False',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              onPressed: () {
+                bool correctAnswer =
+                    quizBrain.questionBank[qNumber].questionAnswer;
+                if (correctAnswer == false) {
+                  print("correct ans");
+                } else {
+                  print("wrong ans");
+                }
+                setState(() {
+                  qNumber++;
+                });
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        ),
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
