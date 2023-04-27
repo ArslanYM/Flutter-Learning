c6a57d160b5dabfb2111767f7bd81b324ae5639c
ArslanYM <arsalanyaqmalik@gmail.com>
2023-04-27 21:24:20 +0530

    added new project

--- Flutter-apps/sneaker_shop/lib/pages/shop.dart
+++ Flutter-apps/sneaker_shop/lib/pages/shop.dart
@@ -13,9 +13,56 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'Shop',
-      style: TextStyle(color: Colors.black),
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 25),
+          padding: EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.grey[100],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Search',
+                style: TextStyle(color: Colors.grey),
+              ),
+              Icon(
+                Icons.search,
+                color: Colors.grey,
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25),
+          child: Text(
+            'everyone flies..... some fly higher than others ',
+            style: TextStyle(color: Colors.grey[600]),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Hot Picks 🔥',
+                style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'See All',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
