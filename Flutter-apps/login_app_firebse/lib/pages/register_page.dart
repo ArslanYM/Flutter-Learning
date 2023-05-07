75bbb6320e04f803e49a1fd1fe54d7e65ff412b4
ArslanYM <arsalanyaqmalik@gmail.com>
2023-05-07 15:09:00 +0530

    feat: Added forgot user functionality

--- Flutter-apps/login_app_firebse/lib/pages/register_page.dart
+++ Flutter-apps/login_app_firebse/lib/pages/register_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -14,16 +15,51 @@ class _RegisterPageState extends State<RegisterPage> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  final _ageController = TextEditingController();
 
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _ageController.dispose();
+    super.dispose();
+  }
+
+  //on signUp
   Future signUp() async {
+    //check if psw matches
     if (passwordMatches()) {
-      FirebaseAuth.instance.createUserWithEmailAndPassword(
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: _emailController.text.trim(),
         password: _passwordController.text.trim(),
+      );
+      //add user details to the database
+      addUserDetails(
+        _firstNameController.text.trim(),
+        _lastNameController.text.toString(),
+        _emailController.text.trim(),
+        int.parse(_ageController.text.trim()),
       );
     }
   }
 
+  // adding usd
+  Future addUserDetails(
+      String firstName, String lastName, String email, int age) async {
+    await FirebaseFirestore.instance.collection('user').add({
+      'first name': firstName,
+      'last name ': lastName,
+      'email': email,
+      'age': age,
+    });
+  }
+
+  //check for matching psw
   bool passwordMatches() {
     if (_passwordController.text.trim() ==
         _confirmPasswordController.text.trim()) {
@@ -42,22 +78,12 @@ class _RegisterPageState extends State<RegisterPage> {
           child: SingleChildScrollView(
             scrollDirection: Axis.vertical,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               //text hello again
               children: [
-                const Icon(
-                  Icons.android,
-                  size: 100,
-                  color: Colors.deepPurple,
-                ),
-                const SizedBox(
-                  height: 75,
-                ),
                 //text hello again
                 Text(
                   'Hello there!',
                   style: GoogleFonts.bebasNeue(fontSize: 52),
-                  // style: ),
                 ),
                 const SizedBox(height: 10),
                 //"welcome back youve been missed!"
@@ -67,13 +93,79 @@ class _RegisterPageState extends State<RegisterPage> {
                 ),
 
                 const SizedBox(height: 50),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Colors.grey.shade100),
+                      color: Colors.grey[100],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: TextField(
+                        controller: _firstNameController,
+                        decoration: InputDecoration(
+                          hintStyle: TextStyle(color: Colors.grey[500]),
+                          hintText: 'First Name',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Colors.grey.shade100),
+                      color: Colors.grey[100],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: TextField(
+                        controller: _lastNameController,
+                        decoration: InputDecoration(
+                          hintStyle: TextStyle(color: Colors.grey[500]),
+                          hintText: 'Last Name',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Colors.grey.shade100),
+                      color: Colors.grey[100],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: TextField(
+                        controller: _ageController,
+                        decoration: InputDecoration(
+                          hintStyle: TextStyle(color: Colors.grey[500]),
+                          hintText: 'Age',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
 
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20),
                   child: Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(12),
-                      border: Border.all(color: Colors.deepPurple),
+                      border: Border.all(color: Colors.grey.shade100),
                       color: Colors.grey[100],
                     ),
                     child: Padding(
@@ -94,9 +186,10 @@ class _RegisterPageState extends State<RegisterPage> {
                   padding: const EdgeInsets.symmetric(horizontal: 20),
                   child: Container(
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.grey[100],
-                        border: Border.all(color: Colors.deepPurple)),
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.grey[100],
+                      border: Border.all(color: Colors.grey.shade100),
+                    ),
                     child: Padding(
                       padding: const EdgeInsets.only(left: 20),
                       child: TextField(
@@ -116,9 +209,10 @@ class _RegisterPageState extends State<RegisterPage> {
                   padding: const EdgeInsets.symmetric(horizontal: 20),
                   child: Container(
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.grey[100],
-                        border: Border.all(color: Colors.deepPurple)),
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.grey[100],
+                      border: Border.all(color: Colors.grey.shade100),
+                    ),
                     child: Padding(
                       padding: const EdgeInsets.only(left: 20),
                       child: TextField(
