010d89e6f2e14450f19d6730867c301189265e0c
ArslanYM <arsalanyaqmalik@gmail.com>
2023-05-09 22:21:26 +0530

    added little styling

--- Flutter-apps/workout_planner/lib/main.dart
+++ Flutter-apps/workout_planner/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:workout_planner/models/workout_data.dart';
 import 'package:workout_planner/pages/home_page.dart';
+import 'package:workout_planner/pages/loading_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         theme: ThemeData(brightness: Brightness.dark),
         debugShowCheckedModeBanner: false,
-        home: HomePage(),
+        home: LoadingPage(),
       ),
     );
   }
---
+++ Flutter-apps/workout_planner/lib/pages/loading_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:workout_planner/pages/home_page.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[850],
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          child: IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomePage(),
+                ),
+              );
+            },
+            icon: Icon(
+              Icons.sports_gymnastics,
+              size: 100,
+            ),
+          ),
+          decoration: BoxDecoration(
+            color: Colors.grey[850],
+            borderRadius: BorderRadius.circular(40),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.shade900,
+                offset: Offset(5.0, 5.0),
+                blurRadius: 15,
+                spreadRadius: 1,
+              ),
+              BoxShadow(
+                color: Colors.grey.shade800,
+                offset: Offset(-5, -5),
+                blurRadius: 15,
+                spreadRadius: 1,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
